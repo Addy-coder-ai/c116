2ab952e0189b09e962d9330244172f6d1d5cbaee
Addy-coder-ai <80392675+Addy-coder-ai@users.noreply.github.com>
2021-07-28 17:59:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 noseX=0;
 noseY=0;
 function preload() {
-   clown_nose= loadImage('https://i.postimg.cc/wjSqdDSK/clownnose.png');
+   clown_nose= loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
 
@@ -21,8 +21,8 @@ function setup() {
 function gotPoses(results) {
     if (results.length>0) {
         console.log(results);
-        noseX=results[0].pose.nose.x;
-        noseY=results[0].pose.nose.y;
+        noseX=results[0].pose.nose.x -20;
+        noseY=results[0].pose.nose.y - 20;
         console.log("nose x = "+noseX);
         console.log("nose y = "+noseY);
     }
